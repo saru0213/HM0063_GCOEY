4872ab89ca3303c5c6c56746f24c9e48e7a98d47
saru0213 <163704968+saru0213@users.noreply.github.com>
2025-03-03 16:11:30 +0530

    Fixed ESLint and dependency issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: "export", // Enables static export mode for Vercel
-//   };
-  
-//   export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true, // Ensures the use of the new App Router
-    },
-    webpack: (config) => {
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-      };
-      return config;
-    },
+    output: "export", // Enables static export mode for Vercel
   };
   
   export default nextConfig;
-  
-  
+
